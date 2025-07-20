34ad280e0dd3c0a3da8dd5c3097e5aa335707f9c
Jcord99 <jcord99@outlook.com>
2025-07-20 17:32:52 -0400

    Create jcord99.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/jcord99.py
@@ -0,0 +1,33 @@
+#OPERADORES
+
+"""
+OPERADORES ARITMETICOS
+"""
+
+print (f"suma: 8+2 = {8+2}")
+print (f"resta: 8-2 = {8-2}")
+print (f"multiplicacion: 8 * 2 = {8*2}")
+print (f"division: 8 / 2 = {8/2}")
+print (f"division entera: 8 // 2 = {8//2}")
+print (f"modulo: 8 % 2 = {8%2}")
+print (f"potencia: 8 ** 2 = {8**2}")
+
+"""
+OPERADORES DE COMPARACION
+"""
+variable_1: true
+variable_2: false
+
+print (f"igualdad: variable_1 == variable_2 {variable_1 == variable_2}")
+print (f"desigualdad: variable_1 != variable_2 {variable_1 != variable_2}")
+print (f"mayor o igual que: 2 >= 1 {2 >= 1}")
+print (f"menor o igual que: 10 <= 15 {10 <= 15}")
+
+"""
+OPERADORES LOGICOS
+"""
+
+print (f"AND &&: 5 + 1 == 6 AND 4 + 1 == 5 {5 + 1 == 6 AND 4 + 1 == 5}")
+print (f"OR ||: 5 + 1 == 6 OR 4 + 1 == 5 {5 + 1 == 6 OR 4 + 1 == 5}")
+print (f"NOT !: 10 + 3 == 14 {10 + 3 == 14}")
+
